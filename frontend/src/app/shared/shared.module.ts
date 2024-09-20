c15c008cb9c445c60d05607a1f2e1536c869ab3a
Llorenç <lloalfsan@gmail.com>
2024-09-20 19:13:19 +0200

    Arquitectura y routing

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './layout/footer/footer.component';
 import { HeaderComponent } from './layout/header/header.component';
-import { HomeComponent } from './home/home.component'; // Importa el componente
+import { HomeComponent } from '../home/home.component'; // Importa el componente
 
 @NgModule({
   declarations: [
     FooterComponent,
-    HomeComponent,
     HeaderComponent
     
   ],
   imports: [
+    HomeComponent,
     CommonModule
   ],
   exports: [
