6cafee647eb24390a37ccc4bb34af731a91aeb08
JavierTomasTormo <javiertomas2003@gmail.com>
2024-09-21 21:20:42 +0200

    Add Shop Jump && Mokkups Shop
    ha añadido las rutas de homa a ahop standalone y ha hecho los mokkups para probar el list shop

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
 
 const routes: Routes = [
   {
@@ -13,10 +14,23 @@ const routes: Routes = [
     component: HomeComponent // COMPONENTE STANDALONE
   },
   {
+    path: 'shop',
+    component: ShopComponent// COMPONENTE STANDALONE???
+  },
+  // {
+  //   path: 'shop',
+  //   loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule)
+  // },
+  // {
+  //   path: 'shop/:id',
+  //   loadChildren: () => import('./shop/product-details/product-details.module').then(m => m.ProductDetailsModule)
+  // },
+  {
     path: '**',
     redirectTo: 'home'
   },
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
