b0c7ec70bcf088b1e40695a3eba62c0acaf14e4c
Llorenç <lloalfsan@gmail.com>
2024-09-24 12:04:20 +0200

    Cargamos el details standalone completo, añadimos el carrousel y su vista y un scroll to top. Cargamos isPlataformBrowser para cuando cargues la url del details no colapse el routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
+// AppRoutingModule.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ShopComponent } from './shop/shop.component';
 import { ListOffertsComponent } from './shared/list-offerts/list-offerts.component';
+import { DetailsComponent } from './details/details.component';
+import { DetailsResolver } from './core/service/details.service';
 
 const routes: Routes = [
   {
@@ -12,29 +15,28 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    component: HomeComponent // COMPONENTE STANDALONE
+    component: HomeComponent
   },
   {
     path: 'shop',
-    component: ShopComponent// COMPONENTE STANDALONE
-  },  
-  { path: 'shop/:slug', component: ListOffertsComponent }, // SALTO DEL HOME AL SHOP CON SLUG CATEGORY
-
-  
-  // {
-  //   path: 'shop',
-  //   loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule)
-  // },
-  // {
-  //   path: 'shop/:id',
-  //   loadChildren: () => import('./shop/product-details/product-details.module').then(m => m.ProductDetailsModule)
-  // },
+    component: ShopComponent
+  },
+  {
+    path: 'shop/:slug',
+    component: ListOffertsComponent
+  },
+  {
+    path: 'details/:slug', // Nueva ruta para detalles de una oferta
+    component: DetailsComponent,
+    resolve: {
+      offer: DetailsResolver // Asociamos el resolver
+    }
+  },
   {
     path: '**',
     redirectTo: 'home'
   },
 ];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/core/models/offert.model.ts
+++ frontend/src/app/core/models/offert.model.ts
@@ -1,16 +1,18 @@
 export interface Offert {
-    _id: string;
-    title: string;
-    company: string;
-    location: string;
-    description: string;
-    requirements: string[];
-    salary: number;
-    slug: string;
-    category: string;
-    categorySlug: string;
-    postedDate: string;
-    image: string;
-    __v: number;
-  }
-  
+  _id: string;
+  title: string;
+  company: string;
+  location: string;
+  description: string;
+  requirements: string[];
+  salary: number;
+  slug: string;
+  category: string;
+  categorySlug: string;
+  postedDate: string;
+  image: string;
+  images?: string[];
+  contractType?: string;
+  experience?: string;
+  __v: number;
+}
