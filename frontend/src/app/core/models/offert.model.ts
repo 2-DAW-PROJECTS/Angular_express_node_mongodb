75f610c55f148854f4b02e16112d0231baacfb8a
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2024-09-30 18:25:03 +0200

    Update offert.model.ts

--- frontend/src/app/core/models/offert.model.ts
+++ frontend/src/app/core/models/offert.model.ts
@@ -1,24 +1,24 @@
 export interface Offert {
-<<<<<<< HEAD
-  _id: string;
-  title: string;
-  company: string;
-  location: string;
-  description: string;
-  requirements: string[];
-  salary: number;
-  slug: string;
-  category: string;
-  categorySlug: string;
-  company_slug: string;
-  postedDate: string;
-  image: string;
-  images?: string[];
-  contractType?: string;
-  experience?: string;
-  __v: number;
-}
-=======
+// <<<<<<< HEAD
+//   _id: string;
+//   title: string;
+//   company: string;
+//   location: string;
+//   description: string;
+//   requirements: string[];
+//   salary: number;
+//   slug: string;
+//   category: string;
+//   categorySlug: string;
+//   company_slug: string;
+//   postedDate: string;
+//   image: string;
+//   images?: string[];
+//   contractType?: string;
+//   experience?: string;
+//   __v: number;
+// }
+// =======
     _id: string;
     title: string;
     company: string;
@@ -34,4 +34,3 @@ export interface Offert {
     __v: number;
   }
   
->>>>>>> parent of fb8a6c6 (Merge pull request #13 from 2-DAW-PROJECTS/alfosan)
