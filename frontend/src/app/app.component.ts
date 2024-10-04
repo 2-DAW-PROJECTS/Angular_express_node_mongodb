a35e40d68806292e6ce53680e78573e4ab61d3f2
Llorenç <lloalfsan@gmail.com>
2024-10-04 12:17:56 +0200

    Pintamos el form del login y register bonito y permanecemos el token en el localstorage.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SharedModule } from './shared/shared.module'; 
+import { UserService } from './core/service/user.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SharedModule], // Importa RouterOutlet para el enrutamiento
+  imports: [RouterOutlet, SharedModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.userService.populate();
+  }
 }
