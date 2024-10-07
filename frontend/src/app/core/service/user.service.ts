f62f9b2d12f3312d50d1da2f63e3bc821a24a6cc
JavierTomasTormo <javiertomas2003@gmail.com>
2024-10-07 12:37:34 +0200

    Debugg Token Timer & Interceptor

--- frontend/src/app/core/service/user.service.ts
+++ frontend/src/app/core/service/user.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Observable, BehaviorSubject, ReplaySubject } from 'rxjs';
+import { Observable, BehaviorSubject, ReplaySubject, interval } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { JwtService } from './jwt.service';
 import { User } from '../models/user.model'; 
-import { map, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { map, distinctUntilChanged, switchMap, takeWhile } from 'rxjs/operators';
 import { environment } from '../../../environments/environment';
 
 
@@ -18,112 +18,74 @@ export class UserService {
   public isAuthenticated = this.isAuthenticatedSubject.asObservable();
 
   private apiUrl = environment.api_url; 
+
+
+  private accessTokenExpirationTimer: any;
+  private refreshTokenExpirationTimer: any;//No toques perro es par saber el temps que falta per a 
+                                    //  caducar el token pa debugg, en com ho toques te arranque la ma
+
+
   constructor (
     private http: HttpClient, 
     private jwtService: JwtService
   ) {}
 
-//   populate() {
-//     const token = this.jwtService.getToken();
-//     // console.log('Token en populate:', token);
 
-//     if (token) {
-//         const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-//         this.http.get(`${this.apiUrl}/user`, { headers }).subscribe(
-//             (data: any) => {
-//                 // console.log('Datos del usuario:', data); 
-//                 if (data && data.user) {
-//                     this.setAuth({ ...data.user, token });
-//                 } else {
-//                     console.error('No se encontró el usuario en la respuesta');
-//                     this.isAuthenticatedSubject.next(false);
-//                     this.currentUserSubject.next(null);
-//                 }
-//             },
-//             (err: any) => {
-//                 console.error('Error al obtener el usuario:', err);
-//                 this.isAuthenticatedSubject.next(false);
-//                 this.currentUserSubject.next(null);
-//             }
-//         );
-//     } else {
-//         this.isAuthenticatedSubject.next(false);
-//         this.currentUserSubject.next(null);
-//     }
-// }
-    populate() {
-      const token = this.jwtService.getAccessToken();  // Sigue utilizando getAccessToken
-
-      if (token) {
-        const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`); // Uso de HttpHeaders con token
-        this.http.get(`${this.apiUrl}/user`, { headers }).subscribe(
-          (data: any) => {
-            if (data && data.user) {
-              this.setAuth({ ...data.user, token });
-            } else {
-              this.purgeAuth();
-            }
-          },
-          () => {
+  populate() {
+    if (this.jwtService.getAccessToken()) {
+      this.http.get(`${this.apiUrl}/user`).subscribe(
+        (data: any) => {
+          if (data && data.user) {
+            this.setAuth(data.user);
+          } else {
             this.purgeAuth();
           }
-        );
-      } else {
-        this.purgeAuth();
-      }
+        },
+        () => {
+          this.purgeAuth();
+        }
+      );
+    } else {
+      this.purgeAuth();
     }
+  }
 
-  // setAuth(user: User) {
-  //   this.jwtService.saveToken(user.token);
-  //   this.currentUserSubject.next(user);
-  //   this.isAuthenticatedSubject.next(true);
-  // }
-
-  // purgeAuth() {
-  //   this.jwtService.destroyToken();
-  //   this.currentUserSubject.next(null);
-  //   this.isAuthenticatedSubject.next(false);
-  // }
 
   setAuth(user: User) {
-    this.jwtService.saveTokens(user.token, user.refreshToken);  // Mantiene el manejo de accessToken y refreshToken
+    this.jwtService.saveTokens(user.token, user.refreshToken);  
     this.currentUserSubject.next(user);
     this.isAuthenticatedSubject.next(true);
+
+
+    this.startTokenExpirationTimers(user.token, user.refreshToken);//debugging
   }
 
   purgeAuth() {
-    this.jwtService.destroyTokens();  // Mantiene la destrucción de ambos tokens
+    this.jwtService.destroyTokens(); 
     this.currentUserSubject.next(null);
     this.isAuthenticatedSubject.next(false);
+  
+  
+    this.stopTokenExpirationTimer();//debugging
   }
 
 
   refreshToken(): Observable<User> {
-    const refreshToken = this.jwtService.getRefreshToken();
-    if (refreshToken) {
-      const headers = new HttpHeaders().set('Authorization', `Bearer ${refreshToken}`);
-      return this.http.post(`${this.apiUrl}/refresh-token`, {}, { headers })
-        .pipe(
-          map((data: any) => {
-            const currentUser = this.getCurrentUser();
-            if (currentUser) {
-              this.setAuth({ 
-                ...currentUser, 
-                token: data.accessToken, 
-                refreshToken: data.refreshToken 
-              });
-              return currentUser;
-            }
-
-            throw new Error('Current user not found');
-          })
-        );
-    } else {
-      return new Observable(observer => {
-        observer.error('No refresh token available');
-      });
-    }
-
+    return this.http.post(`${this.apiUrl}/refresh-token`, {})
+      .pipe(
+        map((data: any) => {
+          const currentUser = this.getCurrentUser();
+          if (currentUser) {
+            this.setAuth({ 
+              ...currentUser, 
+              token: data.accessToken, 
+              refreshToken: data.refreshToken 
+            });
+            return currentUser;
+          }
+          throw new Error('Current user not found');
+        })
+      );
   }
 
 
@@ -141,21 +103,55 @@ export class UserService {
     return this.currentUserSubject.value;
   }
 
-  // update(user: User): Observable<User> {
-  //   return this.http.put(`${this.apiUrl}/user`, { user })
-  //     .pipe(map((data: any) => {
-  //       this.currentUserSubject.next(data.user);
-  //       return data.user;
-  //     }));
-  // }
   update(user: User): Observable<User> {
-    const token = this.jwtService.getAccessToken();
-    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-
-    return this.http.put(`${this.apiUrl}/user`, { user }, { headers })
+    return this.http.put(`${this.apiUrl}/user`, { user })
       .pipe(map((data: any) => {
         this.currentUserSubject.next(data.user);
         return data.user;
       }));
+  }
+
+
+
+
+  /**____________________________DEBUG ZONE____________________________________________ */
+  private startTokenExpirationTimers(accessToken: string, refreshToken: string) {
+    this.stopTokenExpirationTimers();
+    
+    this.startTokenTimer(accessToken, 'Access');
+    this.startTokenTimer(refreshToken, 'Refresh');
+  }
+
+  private startTokenTimer(token: string, tokenType: string) {
+    const tokenPayload = JSON.parse(atob(token.split('.')[1]));
+    const expirationTime = tokenPayload.exp * 1000; // Convert to milliseconds
+    const currentTime = Date.now();
+    const timeUntilExpiration = expirationTime - currentTime;
+
+    if (timeUntilExpiration > 0) {
+      const timer = interval(1000).pipe(
+        takeWhile(() => Date.now() < expirationTime)
+      ).subscribe(() => {
+        const remainingTime = Math.round((expirationTime - Date.now()) / 1000);
+        console.log(`${tokenType} token expires in ${remainingTime} seconds`);
+      });
+
+      if (tokenType === 'Access') {
+        this.accessTokenExpirationTimer = timer;
+      } else {
+        this.refreshTokenExpirationTimer = timer;
+      }
+    } else {
+      console.log(`${tokenType} token has already expired`);
+    }
+  }
+
+  private stopTokenExpirationTimers() {
+    if (this.accessTokenExpirationTimer) {
+      this.accessTokenExpirationTimer.unsubscribe();
+    }
+    if (this.refreshTokenExpirationTimer) {
+      this.refreshTokenExpirationTimer.unsubscribe();
+    }
   }
 }
