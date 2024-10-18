37b105460adaffe9d93a74b04fa5696e8d52a069
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2024-10-18 19:20:13 +0200

    Cambiada la Ruta de .env/ Implementado Prisma en el proyecto

--- prisma/src/routes/userRoutes.ts
+++ prisma/src/routes/userRoutes.ts
@@ -1,10 +1,15 @@
 import { Router, Request, Response, NextFunction } from 'express';
-import { createUser } from '../controllers/userController';
+import { createUser, getUsers } from '../controllers/userController';
 
 const router = Router();
 
 router.post('/', (req: Request, res: Response, next: NextFunction) => {
   createUser(req, res).catch(next);
 });
+
+router.get('/', (req: Request, res: Response, next: NextFunction) => {
+  getUsers(req, res).catch(next);
+});
+
 
 export default router;
