934fa7c2e6c37e2bb3d9248cb4e525f52cdc9c4f
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2024-10-22 15:49:25 +0200

    Update Controller

--- prisma/src/controllers/userEnterpriseController.ts
+++ prisma/src/controllers/userEnterpriseController.ts
@@ -1,25 +1,6 @@
 import { Request, Response } from 'express';
 import prisma from '../services/prismaService';
 
-// export const createUserEnterprise = async (req: Request, res: Response) => {
-//   const { email, name } = req.body;
-
-//   if (!email || !name) {
-//     return res.status(400).json({ error: 'Email and name are required' });
-//   }
-
-//   try {
-//     const userEnterprise = await prisma.userEnterprise.create({
-//       data: { email, name },
-//     });
-
-//     res.status(201).json(userEnterprise);
-//   } catch (error) {
-//     console.error('Error creating user enterprise:', error);
-//     res.status(500).json({ error: 'Error creating user enterprise' });
-//   }
-// };
-
 export const createUserEnterprise = async (req: Request, res: Response) => {
   const {  username, email, password, name, usertype, isActive, permissions, telephone, followers } = req.body;
 
@@ -57,21 +38,6 @@ export const createUserEnterprise = async (req: Request, res: Response) => {
   }
 };
 
-// export const getUserEnterprises = async (req: Request, res: Response) => {
-//   try {
-//     const userEnterprises = await prisma.userEnterprise.findMany({
-//       include: {
-//         posts: true,
-//         comments: true,
-//       },
-//     });
-
-//     res.json(userEnterprises);
-//   } catch (error) {
-//     console.error('Error fetching user enterprises:', error);
-//     res.status(500).json({ error: 'Error fetching user enterprises' });
-//   }
-// };
 export const getUserEnterprises = async (req: Request, res: Response) => {
   try {
     const userEnterprises = await prisma.userEnterprise.findMany({
