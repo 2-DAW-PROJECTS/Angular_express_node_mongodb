bbb393ff26ab5dcc6df8c3e2cfff44335c67866e
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2024-11-05 16:31:46 +0100

    Change button dashboard
    hay una issue que un usuario se crea en enterprise cuando noha sido seleccionado como tal

---
+++ frontend/src/app/enterprise-dashboard/profile-enterprise/profile-enterprise.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-enterprise',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile-enterprise.component.html',
+  styleUrl: './profile-enterprise.component.css'
+})
+export class ProfileEnterpriseComponent {
+
+}
